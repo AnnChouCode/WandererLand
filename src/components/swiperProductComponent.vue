6ebe49a8d9f08fa283dd31d3d00359d2a61b1225
Ann Chou <yuanchou.ann@gmail.com>
2024-03-04 21:25:57 +0800

    fix: 解決 Swiper navigation 失效問題 1. 因 navigation 受 .swiper 的 overflow-hidden 影響，導致超出部分無法顯示，現改變 .swiper 的 margin 和 padding ，擴大顯示區域 2. 刪除多於測試程式碼

--- src/components/swiperProductComponent.vue
+++ src/components/swiperProductComponent.vue
@@ -17,68 +17,91 @@
       }" :autoplay="{
         delay: 5000,
         disableOnInteraction: false,
-      }" :navigation="true" :modules="modules" class="mySwiper">
-      <SwiperSlide v-for="(item, idx) in currentDatas" :key="idx">
-        <router-link :to="`${DataInfo[dataCategory].infoPath}/${item.id}`">
-          <div
-            class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img" :class="{ 'rounded-circle': isArtistBlock }">
-            <img :src="item.imageUrl" :alt="item.title" class="object-fit-contain w-100 h-100 product-img-up">
-            <img :src="item.imagesUrl[0]" alt="item.title"
-              class="position-absolute z-n1 object-fit-contain w-100 h-100 product-img-down">
-          </div>
-        </router-link>
-        <div class="py-3 px-0 px-md-4">
+      }" :navigation="{
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }" :modules="modules" class="mySwiper">
+        <SwiperSlide v-for="( item, idx ) in  currentDatas " :key="idx">
           <router-link :to="`${DataInfo[dataCategory].infoPath}/${item.id}`">
-            <h3 class="mb-2 fs-info fs-md-5 fw-bold text-default" :class="{ 'text-center': isArtistBlock }">{{ item.title }}</h3>
+            <div
+              class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img"
+              :class="{ 'rounded-circle': isArtistBlock }">
+              <img :src="item.imageUrl" :alt="item.title" class="object-fit-contain w-100 h-100 product-img-up">
+              <img :src="item.imagesUrl[0]" alt="item.title"
+                class="position-absolute z-n1 object-fit-contain w-100 h-100 product-img-down">
+            </div>
           </router-link>
-          <div v-if="!isArtistBlock" class="d-flex justify-content-between align-items-center">
-            <p class="fs-info fs-md-6 text-info">NT$ {{ item.price.toLocaleString() }}</p>
-            <button type="button" class="btn p-0">
-              <i class="bi bi-heart"></i>
-            </button>
+          <div class="py-3 px-0 px-md-4">
+            <router-link :to="`${DataInfo[dataCategory].infoPath}/${item.id}`">
+              <h3 class="mb-2 fs-info fs-md-5 fw-bold text-default" :class="{ 'text-center': isArtistBlock }">{{
+        item.title }}</h3>
+            </router-link>
+            <div v-if="!isArtistBlock" class="d-flex justify-content-between align-items-center">
+              <p class="fs-info fs-md-6 text-info">NT$ {{ item.price.toLocaleString() }}</p>
+              <button type="button" class="btn p-0">
+                <i class="bi bi-heart"></i>
+              </button>
+            </div>
           </div>
+        </SwiperSlide>
+        <div class="end-0 z-1 swiper-button-next">
+          <i class="bi bi-arrow-right-circle text-warning fs-1"></i>
         </div>
-      </SwiperSlide>
-    </Swiper>
-    <div class="position-absolute end-0 z-1" style="top:210px">
-      <i class="bi bi-arrow-right-circle text-warning fs-1"></i>
-    </div>
-    <div class="position-absolute z-1" style="top:210px">
-      <i class="position-absolute bi bi-arrow-left-circle text-warning fs-1"></i>
-    </div>
+        <div class="start-0 z-1 swiper-button-prev">
+          <i class="bi bi-arrow-left-circle text-warning fs-1"></i>
+        </div>
+      </Swiper>
     </div>
   </div>
-  <!-- <div class="container py-7 py-md-9">
-  <h2 class="mb-7 mb-md-8 fs-2 fs-md-1">{{ adTitle[adCategoryTitle] }}</h2>
+</template>
 
-  <Swiper
-    :slidesPerView="1"
-    :spaceBetween="48"
-    :pagination="{
-      clickable: true,
-    }"
-    :breakpoints="{
+<!-- <template>
+  <div class="py-7 py-md-9">
+    <div class="d-flex justify-content-between align-items-center mb-7 mb-md-8 ">
+      <h2 class="fs-2 fs-md-1">{{ DataInfo[dataCategory].title }}</h2>
+      <router-link :to="DataInfo[dataCategory].path"
+        class="text-default border-bottom border-default fw-bold fs-info fs-md-6">瀏覽更多</router-link>
+    </div>
+
+    <div class="position-relative">
+      <Swiper :slidesPerView="2" :spaceBetween="12" :pagination="{
+        clickable: true,
+      }" :breakpoints="{
         768: {
           slidesPerView: 3,
+          spaceBetween: 48,
         },
-      }"
-      :autoplay="{
-      delay: 2500,
-      disableOnInteraction: false,
-    }"
-    :navigation="true"
-    :modules="modules"
-    class="mySwiper"
-    >
-      <SwiperSlide v-for="(item, idx) in adCurrentCategory" :key="idx">
-      <router-link :to="`/Datainfo/${item.id}`">
-        <img :src="item.imageUrl" :alt="item.title" class="mb-2">
-      <p class="text-center text-dark">{{ item.title }}</p>
-      </router-link>
-      </SwiperSlide>
-    </Swiper>
-</div> -->
-</template>
+      }" :autoplay="{
+        delay: 5000,
+        disableOnInteraction: false,
+      }" :navigation="true" :modules="modules" class="mySwiper">
+        <SwiperSlide v-for="(item, idx) in currentDatas" :key="idx">
+          <router-link :to="`${DataInfo[dataCategory].infoPath}/${item.id}`">
+            <div
+              class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img"
+              :class="{ 'rounded-circle': isArtistBlock }">
+              <img :src="item.imageUrl" :alt="item.title" class="object-fit-contain w-100 h-100 product-img-up">
+              <img :src="item.imagesUrl[0]" alt="item.title"
+                class="position-absolute z-n1 object-fit-contain w-100 h-100 product-img-down">
+            </div>
+          </router-link>
+          <div class="py-3 px-0 px-md-4">
+            <router-link :to="`${DataInfo[dataCategory].infoPath}/${item.id}`">
+              <h3 class="mb-2 fs-info fs-md-5 fw-bold text-default" :class="{ 'text-center': isArtistBlock }">{{
+        item.title }}</h3>
+            </router-link>
+            <div v-if="!isArtistBlock" class="d-flex justify-content-between align-items-center">
+              <p class="fs-info fs-md-6 text-info">NT$ {{ item.price.toLocaleString() }}</p>
+              <button type="button" class="btn p-0">
+                <i class="bi bi-heart"></i>
+              </button>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  </div>
+</template> -->
 
 <script>
 // import Swiper js
@@ -120,6 +143,13 @@ export default {
     }
   },
   methods: {
+    goNext () {
+      Swiper.slideNext()
+    },
+    goPrev () {
+      Swiper.slidePrev()
+    },
+
     // 取得產品分類列表
     getCurrentDatas () {
       const allData = [...this.allDatas]
