52fa21434c6de9c5d27fc07a4ef64519a73c30c2
Ann Chou <yuanchou.ann@gmail.com>
2024-03-06 10:14:05 +0800

    feat: 新建「加入最愛」元件

---
+++ src/components/btnFavorite.vue
@@ -0,0 +1,30 @@
+<template>
+    <button type="button" class="position-relative btn-favorite btn p-0">
+        <i class="position-absolute bi bi-heart"></i>
+        <i class="bi bi-heart-fill text-info"></i>
+    </button>
+</template>
+
+<style lang="scss">
+.btn-favorite {
+    >i {
+        opacity: 1;
+        transition: opacity 0.3s ease;
+    }
+
+    i:nth-child(2) {
+        opacity: 0;
+        transition: opacity 0.3s ease;
+    }
+
+    &:hover {
+        >i {
+            opacity: 0;
+        }
+
+        i:nth-child(2) {
+            opacity: 1;
+        }
+    }
+}
+</style>
