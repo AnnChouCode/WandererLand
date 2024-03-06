e5aad50a05680183b051aab41425247a5b9a9e91
Ann Chou <yuanchou.ann@gmail.com>
2024-03-06 10:19:10 +0800

    test: 測試產品列表 & 藝術家列表排序 navbar 元件化

--- src/components/userNavBar.vue
+++ src/components/userNavBar.vue
@@ -1,26 +1,68 @@
 <template>
-    <nav class="fixed-top bg-primary">
-    <div class="position-relateive d-flex align-items-center container justify-content-end justify-content-lg-between">
-      <ul class="d-none d-lg-flex align-items-center list-unstyled" >
-        <li><router-link to="/productlist" class="d-block px-5 py-4 text-default">作品</router-link></li>
-        <li><router-link to="/artistlist" class="d-block px-5 py-4 text-default">藝術家</router-link></li>
-        <li><router-link to="/news" class="d-block px-5 py-4 text-default">最新消息</router-link></li>
-      </ul>
-      <h1 class="position-absolute top-50 start-50 translate-middle">
-        <a href="/home" class="d-block h5 fs-md-20 fw-bold text-default">WANDERER LAND</a>
-      </h1>
-      <ul class="d-flex gap-5 align-items-center list-unstyled">
-        <li class="position-relative d-none d-lg-block py-4">
-            <input type="search" class="pe-3 ps-8 rounded-pill border-default form-control bg-transparent fs-info" placeholder="搜尋作品" style="height:28px;">
+  <div class="fixed-top">
+    <nav class="bg-primary">
+      <div class="position-relative d-flex align-items-center container justify-content-end justify-content-lg-between">
+        <ul class="d-none d-lg-flex align-items-center list-unstyled">
+          <li><router-link to="/productlist" class="d-block px-5 py-4 text-default navlink-underline">作品</router-link>
+          </li>
+          <li><router-link to="/artistlist" class="d-block px-5 py-4 text-default navlink-underline">藝術家</router-link>
+          </li>
+          <li><router-link to="/news" class="d-block px-5 py-4 text-default navlink-underline">最新消息</router-link></li>
+        </ul>
+        <h1 class="position-absolute top-50 start-50 translate-middle">
+          <a href="/home" class="d-block h5 fs-md-20 fw-bold text-default">WANDERER LAND</a>
+        </h1>
+        <ul class="d-flex gap-5 align-items-center list-unstyled">
+          <li class="position-relative d-none d-lg-block py-4">
+            <input type="search" class="pe-3 ps-8 rounded-pill border-default form-control bg-transparent fs-info"
+              placeholder="搜尋作品" style="height:28px;">
             <i class="position-absolute start-0 top-50 ms-5 bi bi-search fs-20 translate-middle"></i>
-        </li>
-        <li class="py-4">
+          </li>
+          <li class="py-4">
             <i class="bi bi-heart fs-20"></i>
-        </li>
-        <li class="py-4">
+          </li>
+          <li class="py-4">
             <i class="bi bi-handbag fs-20"></i>
-        </li>
-      </ul>
-    </div>
-  </nav>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <userNavProductFilter v-if="pageType === 'productlist'"></userNavProductFilter>
+    <userNavArtistFilter v-if="pageType === 'artistlist'"></userNavArtistFilter>
+  </div>
 </template>
+
+<script>
+import userNavProductFilter from '@/components/userNavProductFilter.vue'
+import userNavArtistFilter from '@/components/userNavArtistFilter.vue'
+
+export default {
+  data () {
+    return {
+      pageType: ''
+    }
+  },
+  methods: {
+    // 判斷是否為產品頁，顯示對應 navbar
+    checkProductPage () {
+      const url = window.location.href
+      const parts = url.split('/')
+      this.pageType = parts[parts.length - 1]
+      console.log(this.pageType)
+    }
+  },
+  watch: {
+    $route () {
+      this.checkProductPage()
+    }
+  },
+  components: {
+    userNavProductFilter,
+    userNavArtistFilter
+  },
+  mounted () {
+    this.checkProductPage()
+  }
+}
+</script>
---
+++ src/components/userNavProductFilter.vue
@@ -0,0 +1,88 @@
+<template>
+    <nav class="bg-tertiary nav-product-filter">
+        <div class="container">
+            <!-- web -->
+            <ul class="d-none d-lg-flex align-items-center list-unstyled">
+                <li>
+                    <div class="dropdown">
+                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                            id="btnProductSort" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="me-3">排序</span>
+                            <i class="bi bi-chevron-down"></i>
+                        </button>
+                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnProductSort">
+                            <li><a class="dropdown-item p-3" href="#">最新作品</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：高至低</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：低至高</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div class="dropdown">
+                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                            id="btnSizeFilter" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="me-3">尺寸</span>
+                            <i class="bi bi-chevron-down"></i>
+                        </button>
+                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnSizeFilter">
+                            <li><a class="dropdown-item p-3" href="#">最新作品</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：高至低</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：低至高</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div class="dropdown">
+                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                            id="btnGroupFilter" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="me-3">題材</span>
+                            <i class="bi bi-chevron-down"></i>
+                        </button>
+                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnGroupFilter">
+                            <li><a class="dropdown-item p-3" href="#">最新作品</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：高至低</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：低至高</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <div class="dropdown">
+                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                            id="btnArtistFilter" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="me-3">藝術家</span>
+                            <i class="bi bi-chevron-down"></i>
+                        </button>
+                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnArtistFilter">
+                            <li><a class="dropdown-item p-3" href="#">最新作品</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：高至低</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：低至高</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+            <!-- mobile -->
+            <ul class="d-flex d-lg-none align-items-center justify-content-between list-unstyled">
+                <li>
+                    <div class="dropdown">
+                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                            id="btnProductSort" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span class="me-3">排序</span>
+                            <i class="bi bi-chevron-down"></i>
+                        </button>
+                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnProductSort">
+                            <li><a class="dropdown-item p-3" href="#">最新作品</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：高至低</a></li>
+                            <li><a class="dropdown-item p-3" href="#">價格：低至高</a></li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0">
+                        <span class="me-3">篩選</span>
+                        <i class="bi bi-chevron-down"></i>
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
