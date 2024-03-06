9375838a5f50c5495029de12814e8b2e69325a5c
Ann Chou <yuanchou.ann@gmail.com>
2024-03-06 10:22:47 +0800

    refactor: 整理程式碼

--- src/components/toastComponent.vue
+++ src/components/toastComponent.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
-      <div class="toast show mb-1" role="alert" aria-live="assertive" aria-atomic="true" v-for="(item, idx) in toastMsgList" :key="idx">
-        <div class="toast-body d-flex align-items-center justify-content-between">
-            <div class="d-flex">
-              <i class="bi me-2 text-success" :class="item.icon"></i>
-               <p class="mb-0">{{item.msg}}</p>
-            </div>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"
-          @click="clearToast(idx)"></button>
+  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+    <div class="toast show mb-1" role="alert" aria-live="assertive" aria-atomic="true"
+      v-for="(item, idx) in toastMsgList" :key="idx">
+      <div class="toast-body d-flex align-items-center justify-content-between">
+        <div class="d-flex">
+          <i class="bi me-2 text-success" :class="item.icon"></i>
+          <p class="mb-0">{{ item.msg }}</p>
         </div>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"
+          @click="clearToast(idx)"></button>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
