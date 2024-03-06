5908caeabaeda369dc26e44a3a693ca3bb78ca63
Ann Chou <yuanchou.ann@gmail.com>
2024-03-06 10:18:33 +0800

    test: 測試新增藝術家列表 & 藝術家資訊頁

---
+++ src/views/user/UserArtistInfo.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="container user-page-container" v-if="product.productInfo">
+    <div class="row g-1 g-lg-6">
+      <!-- 圖片 -->
+      <div class="col-12 col-md-8">
+        <div class="row">
+          <div class="position-relative col-12 col-lg-3">
+            <div class="bg-tertiary h-100">
+              <div
+                class="position-lg-absolute row gap-0 gap-lg-2 px-2 py-2 py-lg-0 px-lg-auto flex-nowrap flex-lg-wrap overflow-auto scrollbar-y-hide w-lg-100 h-lg-100"
+                v-if="product.imagesStock">
+                <a href="#" class="col-3 col-lg-12 ratio-1x1 overflow-hidden" v-for="(img, idx) in product.imagesStock"
+                  :key="'img' + idx" @click.prevent="changeImage(idx)">
+                  <img :src="img" alt="product" class="object-fit-contain w-100 h-100">
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-9 order-first order-lg-last flex-shrink-0 content">
+            <div class="bg-tertiary ratio-1x1 overflow-hidden">
+              <img :src="product.currentImage" :alt="product.productInfo.title" class="object-fit-contain w-100 h-100">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 介紹 -->
+      <div class="col-12 col-md-4">
+        <div class="p-3 p-lg-7 mb-3 mb-md-6 bg-tertiary">
+          <div class="d-flex justify-content-between align-items-start mb-5 mb-md-7">
+            <div>
+              <h2 class="h5 fw-bold">{{ product.productInfo.title }}</h2>
+            </div>
+            <btnFavorite></btnFavorite>
+          </div>
+          <p class="lh-base lh-md-lg fs-info text-info">{{ product.productInfo.content }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container" v-if="relatedProducts.length">
+    <div class="py-7 py-md-9">
+      <div class="d-flex justify-content-between align-items-center mb-7 mb-md-8 ">
+        <h2 class="fs-2 fs-md-1">相似作品</h2>
+        <router-link to="/productlist"
+          class="text-default border-bottom border-default fw-bold fs-info fs-md-6">瀏覽更多</router-link>
+      </div>
+
+      <div class="row g-3 g-md-8">
+        <div class="col-6 col-md-4" v-for="product in relatedProducts" :key="product.id">
+          <router-link :to="`/productinfo/${product.id}`">
+            <div
+              class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img">
+              <img :src="product.imageUrl" :alt="product.title" class="object-fit-contain w-100 h-100 product-img-up">
+              <img :src="product.imagesUrl[0]" alt="item.title"
+                class="position-absolute z-n1 object-fit-contain w-100 h-100 product-img-down">
+            </div>
+          </router-link>
+          <div class="py-3 px-0 px-md-4">
+            <router-link :to="`/productinfo/${product.id}`">
+              <h3 class="mb-2 fs-info fs-md-5 fw-bold text-default">{{ product.title }}</h3>
+            </router-link>
+            <div v-if="!isArtistBlock" class="d-flex justify-content-between align-items-center">
+              <p class="fs-info fs-md-6 text-info">NT$ {{ product.price.toLocaleString() }}</p>
+              <btnFavorite></btnFavorite>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import userProductStore from '@/stores/userProductStore.js'
+import cartStore from '@/stores/cartStore.js'
+import { mapActions, mapState } from 'pinia'
+
+// Import Components
+import btnFavorite from '@/components/btnFavorite.vue'
+
+const { VITE_API, VITE_PATH } = import.meta.env
+
+export default {
+  data () {
+    return {
+      // 產品資訊
+      product: {},
+      // 產品數量
+      qty: 1,
+      // 相似產品
+      relatedProducts: [],
+      // 藝術家資訊
+      artistInfo: {}
+    }
+  },
+  methods: {
+    // 取得所有產品資料，生成產品與分類資料
+    ...mapActions(userProductStore, ['getAllProducts']),
+
+    // 加入購物車
+    ...mapActions(cartStore, ['addToCart']),
+
+    // 切換顯示大圖
+    changeImage (idx) {
+      this.product.currentImage = this.product.imagesStock[idx]
+    },
+
+    // 獲取當頁產品資料
+    getCurrentProduct (id) {
+      const url = `${VITE_API}/api/${VITE_PATH}/product/${id}`
+
+      // 獲取當頁產品資料
+      return this.axios.get(url)
+        .then(res => {
+          const resData = res.data.product
+          this.product = {
+            productInfo: resData,
+            currentImage: resData.imageUrl
+          }
+
+          if (resData.imagesUrl) {
+            this.product.imagesStock = [resData.imageUrl, ...resData.imagesUrl]
+          }
+
+          return resData.artist
+        })
+        .catch(err => console.log(err.response.data.message))
+    },
+
+    // 獲取 3 件相似作品
+    getRelatedProducts (artist, id) {
+      // 獲取藝術家的作品，不包含當前作品
+      let relatedAllProducts = this.sortNewest.newestProduct.filter(item => item.artist === artist && item.id !== id)
+      // 如果相似作品清單為 0，則改撈取相同題材作品
+      if (!relatedAllProducts.length) {
+        relatedAllProducts = this.sortNewest.newestProduct.filter(item => item.group === this.product.productInfo.group)
+      }
+
+      this.relatedProducts = relatedAllProducts.slice(0, 3)
+    },
+
+    // 獲取藝術家資料
+    getArtistInfo (artist) {
+      this.artistInfo = this.sortNewest.newestArtist.filter(item => item.title === artist)[0]
+    }
+  },
+  async mounted () {
+    try {
+      const id = this.$route.params.id
+
+      const currendProductArtist = await this.getCurrentProduct(id)
+      await this.getAllProducts()
+
+      this.getRelatedProducts(currendProductArtist, id)
+      this.getArtistInfo(currendProductArtist)
+    } catch (err) {
+      console.log('錯誤:', err)
+      throw err
+    }
+  },
+  computed: {
+    ...mapState(userProductStore, ['sortNewest'])
+  },
+  components: {
+    btnFavorite
+  }
+}
+</script>
---
+++ src/views/user/UserArtistList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container user-product-page-container">
+    <h2 class="mb-7 mb-md-8 h1 lh-sm title-letter-spacing text-center">藝術家</h2>
+    <div class="row  g-3 g-md-8">
+      <div class="col-6 col-md-4" v-for="item in sortNewest.newestArtist" :key="item.id">
+        <router-link :to="`artistInfo/${item.id}`">
+          <div
+            class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img rounded-circle">
+            <img :src="item.imageUrl" :alt="item.title" class="object-fit-contain w-100 h-100 product-img-up">
+            <img :src="item.imagesUrl[0]" alt="item.title"
+              class="position-absolute z-n1 object-fit-contain w-100 h-100 product-img-down">
+          </div>
+          <div class="py-3 px-0 px-md-4">
+            <h3 class="mb-2 fs-info fs-md-5 fw-bold text-default text-center">{{ item.title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import userProductStore from '@/stores/userProductStore.js'
+import { mapActions, mapState } from 'pinia'
+
+export default {
+  data () {
+    return {
+      // 產品列表
+      productsList: [],
+      // 頁碼
+      pagination: {}
+    }
+  },
+  methods: {
+    // 取得所有產品資料，生成產品與分類資料
+    ...mapActions(userProductStore, ['getAllProducts']),
+
+    // 切換產品資訊頁
+    showProductInfo (id) {
+      this.$router.push(`/productinfo/${id}`)
+    }
+  },
+  mounted () {
+    // 取得分頁產品列表
+    // this.getProductList()
+    // 取得所有產品資料
+    this.getAllProducts()
+  },
+  computed: {
+    ...mapState(userProductStore, ['sortNewest'])
+  },
+  components: {
+    // userNavProductFilter
+    // paginationComponent,
+    // filterCategory
+  }
+}
+</script>
