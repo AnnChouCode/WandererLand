6185b6225f43589340438195722e9945dda42c3a
Ann Chou <yuanchou.ann@gmail.com>
2024-03-06 10:22:17 +0800

    feat: 建立產品列表、產品資訊頁功能 1. 撈取藝術家資料目前以藝術家姓名為依據，未來考慮改成使用 ID

--- src/views/user/UserProductInfo.vue
+++ src/views/user/UserProductInfo.vue
@@ -1,43 +1,126 @@
 <template>
-    <div class="container" v-if="product.productInfo">
-        <h2 class="py-5 h2 text-center">{{product.productInfo.title}}</h2>
-        <div>
-               <div class="row">
-                 <div class="col-sm-6">
-                     <!-- 主圖 -->
-                    <img class="d-block img-fluid mx-auto" :src="product.currentImage" :alt="product" style="max-height:500px;">
-                    <!-- 圖片庫 -->
-                    <div v-if="product.imagesStock">
-                        <a href="#" v-for="(img, idx) in product.imagesStock" :key="'img' + idx" @click.prevent="changeImage(idx)">
-                        <img :src="img" alt="productImg" class="img-store m-1" :class="img === product.currentImage ? 'border border-3 border-primary' : '' ">
-                        </a>
-                    </div>
-                 </div>
-                    <div class="col-sm-6">
-                    <span class="mb-4 badge bg-dark rounded-pill">{{ product.productInfo.category }}</span>
-                    <p class="mb-2">商品描述：{{ product.productInfo.description }}</p>
-                    <p class="mb-4">商品內容：{{ product.productInfo.content }}</p>
-                    <div class="mb-4">
-                        <div class="h5" v-if="!product.productInfo.price">{{ product.productInfo.origin_price }} 元</div>
-                        <del class="h6" v-if="product.productInfo.price">原價 {{ product.productInfo.origin_price }} 元</del>
-                        <div class="h5" v-if="product.productInfo.price">現在只要 {{ product.productInfo.price }} 元</div>
-                    </div>
-                    <div>
-                        <div class="input-group">
-                        <input type="number" class="form-control"
-                        v-model.number="qty" min="1">
-                        <button type="button" class="btn btn-dark" @click="addToCart(product.productInfo.id, qty)">加入購物車</button>
-                        </div>
-                    </div>
-                </div>
+  <div class="container user-page-container" v-if="product.productInfo">
+    <div class="row g-1 g-lg-6">
+      <!-- 圖片 -->
+      <div class="col-12 col-md-8">
+        <div class="row">
+          <div class="position-relative col-12 col-lg-3">
+            <div class="bg-tertiary h-100">
+              <div
+                class="position-lg-absolute row gap-0 gap-lg-2 px-2 py-2 py-lg-0 px-lg-auto flex-nowrap flex-lg-wrap overflow-auto scrollbar-y-hide w-lg-100 h-lg-100"
+                v-if="product.imagesStock">
+                <a href="#" class="col-3 col-lg-12 ratio-1x1 overflow-hidden" v-for="(img, idx) in product.imagesStock"
+                  :key="'img' + idx" @click.prevent="changeImage(idx)">
+                  <img :src="img" alt="product" class="object-fit-contain w-100 h-100">
+                </a>
+              </div>
             </div>
+          </div>
+          <div class="col-12 col-lg-9 order-first order-lg-last flex-shrink-0 content">
+            <div class="bg-tertiary ratio-1x1 overflow-hidden">
+              <img :src="product.currentImage" :alt="product.productInfo.title" class="object-fit-contain w-100 h-100">
+            </div>
+          </div>
         </div>
+      </div>
+      <!-- 介紹 -->
+      <div class="col-12 col-md-4">
+        <div class="p-3 p-lg-7 mb-3 mb-md-6 bg-tertiary">
+          <div class="d-flex justify-content-between align-items-start mb-5 mb-md-7">
+            <div>
+              <h3 class="mb-2 h6 text-info fw-bold">{{ product.productInfo.artist }}</h3>
+              <h2 class="h5 fw-bold">{{ product.productInfo.title }}</h2>
+            </div>
+            <btnFavorite></btnFavorite>
+          </div>
+          <p class="lh-base lh-md-lg fs-info text-info">{{ product.productInfo.content }}
+          </p>
+          <hr class="my-6 text-default">
+          <!-- <div class="py-6 w-100 border-bottom border-default"></div> -->
+          <div class="accordion accordion-flush" id="accordionPanelsProductInfo">
+            <div class="accordion-item bg-transparent border-default">
+              <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                <button class="accordion-button text-info fw-bold bg-transparent p-0" type="button"
+                  data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                  aria-controls="panelsStayOpen-collapseOne" style="box-shadow: none;">
+                  作品規格
+                </button>
+              </h2>
+              <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
+                aria-labelledby="panelsStayOpen-headingOne">
+                <div class="accordion-body p-0 pt-4 lh-base lh-md-lg h6 text-info">
+                  {{ product.productInfo.size }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr class="my-6 text-default">
+          <!-- <div class="py-6 w-100 border-bottom border-default"></div> -->
+          <div class="accordion accordion-flush" id="accordionPanelsProductInfo">
+            <div class="accordion-item bg-transparent">
+              <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                <button class="accordion-button text-info fw-bold bg-transparent p-0" type="button"
+                  data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="true"
+                  aria-controls="panelsStayOpen-collapseTwo" style="box-shadow: none;">
+                  關於 {{ artistInfo.title }}
+                </button>
+              </h2>
+              <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse"
+                aria-labelledby="panelsStayOpen-headingOne">
+                <div class="accordion-body p-0 pt-4 lh-base lh-md-lg fs-info text-info">
+                  {{ artistInfo.content }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button type="button" class="py-2 py-md-3 btn btn-default rounded-0 fw-bold w-100"
+          @click="addToCart(product.productInfo.id, qty)">加入購物車．NT$ {{
+    product.productInfo.price.toLocaleString() }}</button>
+      </div>
     </div>
+  </div>
+
+  <div class="container" v-if="relatedProducts.length">
+    <div class="py-7 py-md-9">
+      <div class="d-flex justify-content-between align-items-center mb-7 mb-md-8 ">
+        <h2 class="fs-2 fs-md-1">相似作品</h2>
+        <router-link to="/productlist"
+          class="text-default border-bottom border-default fw-bold fs-info fs-md-6">瀏覽更多</router-link>
+      </div>
+
+      <div class="row g-3 g-md-8">
+        <div class="col-6 col-md-4" v-for="product in relatedProducts" :key="product.id">
+          <router-link :to="`/productinfo/${product.id}`">
+            <div
+              class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img">
+              <img :src="product.imageUrl" :alt="product.title" class="object-fit-contain w-100 h-100 product-img-up">
+              <img :src="product.imagesUrl[0]" alt="item.title"
+                class="position-absolute z-n1 object-fit-contain w-100 h-100 product-img-down">
+            </div>
+          </router-link>
+          <div class="py-3 px-0 px-md-4">
+            <router-link :to="`/productinfo/${product.id}`">
+              <h3 class="mb-2 fs-info fs-md-5 fw-bold text-default">{{ product.title }}</h3>
+            </router-link>
+            <div v-if="!isArtistBlock" class="d-flex justify-content-between align-items-center">
+              <p class="fs-info fs-md-6 text-info">NT$ {{ product.price.toLocaleString() }}</p>
+              <btnFavorite></btnFavorite>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import userProductStore from '@/stores/userProductStore.js'
 import cartStore from '@/stores/cartStore.js'
-import { mapActions } from 'pinia'
+import { mapActions, mapState } from 'pinia'
+
+// Import Components
+import btnFavorite from '@/components/btnFavorite.vue'
 
 const { VITE_API, VITE_PATH } = import.meta.env
 
@@ -47,37 +130,83 @@ export default {
       // 產品資訊
       product: {},
       // 產品數量
-      qty: 1
+      qty: 1,
+      // 相似產品
+      relatedProducts: [],
+      // 藝術家資訊
+      artistInfo: {}
     }
   },
   methods: {
+    // 取得所有產品資料，生成產品與分類資料
+    ...mapActions(userProductStore, ['getAllProducts']),
+
     // 加入購物車
     ...mapActions(cartStore, ['addToCart']),
 
     // 切換顯示大圖
     changeImage (idx) {
       this.product.currentImage = this.product.imagesStock[idx]
-    }
-  },
-  mounted () {
-    const id = this.$route.params.id
-
-    const url = `${VITE_API}/api/${VITE_PATH}/product/${id}`
+    },
 
     // 獲取當頁產品資料
-    this.axios.get(url)
-      .then(res => {
-        const resData = res.data.product
-        this.product = {
-          productInfo: resData,
-          currentImage: resData.imageUrl
-        }
+    getCurrentProduct (id) {
+      const url = `${VITE_API}/api/${VITE_PATH}/product/${id}`
 
-        if (resData.imagesUrl) {
-          this.product.imagesStock = [resData.imageUrl, ...resData.imagesUrl]
-        }
-      })
-      .catch(err => console.log(err.response.data.message))
+      // 獲取當頁產品資料
+      return this.axios.get(url)
+        .then(res => {
+          const resData = res.data.product
+          this.product = {
+            productInfo: resData,
+            currentImage: resData.imageUrl
+          }
+
+          if (resData.imagesUrl) {
+            this.product.imagesStock = [resData.imageUrl, ...resData.imagesUrl]
+          }
+
+          return resData.artist
+        })
+        .catch(err => console.log(err.response.data.message))
+    },
+
+    // 獲取 3 件相似作品
+    getRelatedProducts (artist, id) {
+      // 獲取藝術家的作品，不包含當前作品
+      let relatedAllProducts = this.sortNewest.newestProduct.filter(item => item.artist === artist && item.id !== id)
+      // 如果相似作品清單為 0，則改撈取相同題材作品
+      if (!relatedAllProducts.length) {
+        relatedAllProducts = this.sortNewest.newestProduct.filter(item => item.group === this.product.productInfo.group)
+      }
+
+      this.relatedProducts = relatedAllProducts.slice(0, 3)
+    },
+
+    // 獲取藝術家資料
+    getArtistInfo (artist) {
+      this.artistInfo = this.sortNewest.newestArtist.filter(item => item.title === artist)[0]
+    }
+  },
+  async mounted () {
+    try {
+      const id = this.$route.params.id
+
+      const currendProductArtist = await this.getCurrentProduct(id)
+      await this.getAllProducts()
+
+      this.getRelatedProducts(currendProductArtist, id)
+      this.getArtistInfo(currendProductArtist)
+    } catch (err) {
+      console.log('錯誤:', err)
+      throw err
+    }
+  },
+  computed: {
+    ...mapState(userProductStore, ['sortNewest'])
+  },
+  components: {
+    btnFavorite
   }
 }
 </script>
--- src/views/user/UserProductList.vue
+++ src/views/user/UserProductList.vue
@@ -1,61 +1,46 @@
 <template>
-  <div class="container">
-    <h2 class="py-5 h2 text-center">看產品</h2>
-    <!-- 分類篩選 -->
-    <filterCategory @get-Product-List="getProductList"></filterCategory>
-    <!-- 產品列表 -->
-    <table class="table align-middle">
-      <thead>
-        <tr>
-          <th>圖片</th>
-          <th>商品名稱</th>
-          <th>價格</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="product in productsList" :key="product.id">
-          <td style="width: 200px">
-            <div style="height: 100px; background-size: cover; background-position: center;"
-              :style="{ backgroundImage: `url(${product.imageUrl})` }">
-            </div>
-          </td>
-          <td>
-            {{ product.title }}
-          </td>
-          <td>
-            <div class="h5" v-if="!product.price">{{ product.origin_price }} 元</div>
-            <div v-else>
-              <del class="h6">原價 {{ product.origin_price }} 元</del>
-              <div class="h5">現在只要 {{ product.price }} 元</div>
-            </div>
-          </td>
-          <td>
-            <div class="btn-group btn-group-sm">
-              <button type="button" class="btn btn-outline-default" @click="showProductInfo(product.id)">
-                查看更多
-              </button>
-              <button type="button" class="btn btn-default" @click="addToCart(product.id, 1)">
-                加到購物車
-              </button>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <!-- 頁碼 -->
-    <paginationComponent :pagination="pagination" @get-List="getProductList"></paginationComponent>
+  <!-- <userNavProductFilter></userNavProductFilter> -->
+
+  <div class="container user-product-page-container">
+    <h2 class="mb-7 mb-md-8 h1 lh-sm title-letter-spacing text-center">作品</h2>
+    <div class="row  g-3 g-md-8">
+      <div class="col-6 col-md-4" v-for="item in sortNewest.newestProduct" :key="item.id">
+        <router-link :to="`productInfo/${item.id}`">
+          <div
+            class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img">
+            <img :src="item.imageUrl" :alt="item.title" class="object-fit-contain w-100 h-100 product-img-up">
+            <img :src="item.imagesUrl[0]" alt="item.title"
+              class="position-absolute z-n1 object-fit-contain w-100 h-100 product-img-down">
+          </div>
+        </router-link>
+        <div class="py-3 px-0 px-md-4">
+          <router-link :to="`productInfo/${item.id}`">
+            <h3 class="mb-2 fs-info fs-md-5 fw-bold text-default">{{
+        item.title }}</h3>
+          </router-link>
+          <div class="d-flex justify-content-between align-items-center">
+            <p class="fs-info fs-md-6 text-info">NT$ {{ item.price.toLocaleString() }}</p>
+            <btnFavorite></btnFavorite>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import cartStore from '@/stores/cartStore.js'
-import allProductStore from '@/stores/allProductStore.js'
-import { mapActions } from 'pinia'
-import paginationComponent from '@/components/paginationComponent.vue'
-import filterCategory from '@/components/filterCategory.vue'
+// import cartStore from '@/stores/cartStore.js'
+import userProductStore from '@/stores/userProductStore.js'
+import { mapActions, mapState } from 'pinia'
 
-const { VITE_API, VITE_PATH } = import.meta.env
+// Import Components
+import btnFavorite from '@/components/btnFavorite.vue'
+
+// import userNavProductFilter from '@/components/userNavProductFilter.vue'
+// import paginationComponent from '@/components/paginationComponent.vue'
+// import filterCategory from '@/components/filterCategory.vue'
+
+// const { VITE_API, VITE_PATH } = import.meta.env
 
 export default {
   data () {
@@ -67,31 +52,8 @@ export default {
     }
   },
   methods: {
-    // 加入購物車
-    ...mapActions(cartStore, ['addToCart']),
     // 取得所有產品資料，生成產品與分類資料
-    ...mapActions(allProductStore, ['getAllProducts']),
-
-    // 取得分頁產品列表
-    getProductList (page = 1, category) {
-      let url = `${VITE_API}/api/${VITE_PATH}/products?page=${page}`
-      if (category) { url += `&category=${category}` }
-
-      this.axios.get(url)
-        .then(res => {
-          const { products, pagination } = res.data
-          this.productsList = products
-          this.pagination = pagination
-        })
-        .catch(err => {
-          this.$swal.fire(
-            {
-              icon: 'error',
-              text: err.response.data.message
-            }
-          )
-        })
-    },
+    ...mapActions(userProductStore, ['getAllProducts']),
 
     // 切換產品資訊頁
     showProductInfo (id) {
@@ -100,13 +62,18 @@ export default {
   },
   mounted () {
     // 取得分頁產品列表
-    this.getProductList()
+    // this.getProductList()
     // 取得所有產品資料
     this.getAllProducts()
   },
+  computed: {
+    ...mapState(userProductStore, ['sortNewest'])
+  },
   components: {
-    paginationComponent,
-    filterCategory
+    btnFavorite
+    // userNavProductFilter
+    // paginationComponent,
+    // filterCategory
   }
 }
 </script>
