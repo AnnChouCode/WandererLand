b48d2f1158dca56e520cdaec3a66507d24cd5b1e
Ann Chou <yuanchou.ann@gmail.com>
2024-03-07 11:42:37 +0800

    feat: 建立訂購者表格 1. 訂購成功後將引導用戶至指定訂單 id 的結帳頁面

---
+++ src/views/user/UserCheckOrderForm.vue
@@ -0,0 +1,127 @@
+<template>
+    <v-form ref="form" v-slot="{ errors }" class="d-flex flex-column align-items-end" @submit="checkOrder">
+        <div class="mb-4 mb-md-7 form-floating w-100">
+            <v-field id="email" name="email" type="email" class="form-control border-info rounded-0"
+                :class="{ 'is-invalid': errors['Email'] }" placeholder="請輸入 Email" rules="email|required"
+                v-model="form.user.email"></v-field>
+            <error-message name="Email" class="invalid-feedback"></error-message>
+            <label for="Email" class="form-label has-required">Email</label>
+        </div>
+
+        <div class="mb-4 mb-md-7 form-floating w-100">
+            <v-field id="name" name="姓名" type="text" class="form-control border-info rounded-0"
+                :class="{ 'is-invalid': errors['姓名'] }" placeholder="請輸入姓名" rules="required"
+                v-model="form.user.name"></v-field>
+            <error-message name="姓名" class="invalid-feedback"></error-message>
+            <label for="name" class="form-label has-required">收件人姓名</label>
+        </div>
+
+        <div class="mb-4 mb-md-7 form-floating w-100">
+            <v-field id="tel" name="電話" type="tel" class="form-control border-info rounded-0"
+                :class="{ 'is-invalid': errors['電話'] }" placeholder="請輸入電話" rules="required|min:8|max:10"
+                v-model="form.user.tel"></v-field>
+            <error-message name="電話" class="invalid-feedback"></error-message>
+            <label for="tel" class="form-label has-required">收件人電話</label>
+        </div>
+
+        <div class="mb-4 mb-md-7 form-floating w-100">
+            <v-field id="address" name="地址" type="text" class="form-control border-info rounded-0"
+                :class="{ 'is-invalid': errors['地址'] }" placeholder="請輸入地址" rules="required"
+                v-model="form.user.address"></v-field>
+            <error-message name="地址" class="invalid-feedback"></error-message>
+            <label for="address" class="form-label has-required">收件人地址</label>
+        </div>
+
+        <div class="mb-4 mb-md-7 form-floating w-100">
+            <v-field id="message" name="message" class="form-control border-info rounded-0" cols="30" rows="10"
+                v-model="form.message" as="textarea" placeholder="請輸入您的留言" style="height:273px"></v-field>
+            <label for="message" class="form-label">留言</label>
+        </div>
+        <button class="py-2 py-md-3 px-6 px-md-9 btn btn-default fw-bold rounded-0 w-100 w-md-auto" type="submit">
+            訂單確認
+        </button>
+    </v-form>
+</template>
+
+<script>
+import cartStore from '@/stores/cartStore.js'
+import { mapActions, mapState } from 'pinia'
+
+const { VITE_API, VITE_PATH } = import.meta.env
+
+export default {
+  data () {
+    return {
+      // 訂單欄位
+      form: {
+        user: {
+          email: '',
+          tel: '',
+          address: ''
+        },
+        message: ''
+      }
+    }
+  },
+  methods: {
+    // 獲得購物車資料
+    ...mapActions(cartStore, ['getCartsList']),
+
+    // 確認訂單
+    checkOrder () {
+      // 開啟 loading
+      const loader = this.$loading.show()
+
+      // 資料處理
+      const url = `${VITE_API}/api/${VITE_PATH}/order`
+      const order = {
+        data: this.form
+      }
+
+      this.axios.post(url, order)
+        .then(res => {
+          // 提示訊息
+          this.swalInfoCheckWithBootstrapButtons.fire({
+            title: `<p> ${res.data.message}</p><p>訂單編號 </p><p>${res.data.orderId}</p>`,
+            confirmButtonText: '確認'
+          })
+
+          // 清除表單
+          this.$refs.form.resetForm()
+          console.log('清除表單')
+          // 重整購物車
+          this.getCartsList()
+          console.log('重整購物車')
+          console.log('頁面跳轉', `/order/payorder/${res.data.orderId}`)
+          // 頁面跳轉
+          this.$router.push(`/order/payorder/${res.data.orderId}`)
+        })
+        .catch(err => {
+          this.swalInfoCheckWithBootstrapButtons.fire(
+            {
+              icon: 'error',
+              text: err.response.data.message,
+              confirmButtonText: '確認'
+            }
+          )
+        })
+        .finally(() => {
+          // 關閉 loading
+          loader.hide()
+        })
+    }
+  },
+  mounted () {
+    this.getCartsList()
+
+    // 客製化 info check alert 按鈕
+    this.swalInfoCheckWithBootstrapButtons = this.$swal.mixin({
+      customClass: {
+        confirmButton: 'm-1 btn btn-default'
+      },
+      buttonsStyling: false
+    })
+  },
+  computed: { ...mapState(cartStore, ['cartsList']) }
+}
+</script>
