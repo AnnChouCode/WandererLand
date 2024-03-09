f1369b24ba693c2391e6ed8fca00e64aa5499306
Ann Chou <yuanchou.ann@gmail.com>
2024-03-09 17:59:24 +0800

    refactor: 刪除多餘 return

--- src/stores/userArticleStore.js
+++ src/stores/userArticleStore.js
@@ -13,11 +13,9 @@ export default defineStore('articleStore', {
     getArticlesList () {
       // 資料處理
       const url = `${VITE_API}/api/${VITE_PATH}/articles`
-      return axios.get(url)
+      axios.get(url)
         .then(res => {
           this.articlesList = res.data.articles
-          console.log('this.articlesList', this.articlesList)
-          return res
         })
         .catch(err => {
           console.log(err.response.data.message)
