9628a8b764d81a0098aa6dcf073285a48739edcf
Ann Chou <yuanchou.ann@gmail.com>
2024-03-09 08:51:55 +0800

    Updates

---
+++ assets/btnFavorite-utzbfxiw.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,d as t}from"./index-f4rJqI21.js";const n={},i={type:"button",class:"position-relative btn-favorite btn p-0"},c=t("i",{class:"position-absolute bi bi-heart"},null,-1),a=t("i",{class:"bi bi-heart-fill text-info"},null,-1),r=[c,a];function _(l,b){return o(),s("button",i,r)}const f=e(n,[["render",_]]);export{f as b};
