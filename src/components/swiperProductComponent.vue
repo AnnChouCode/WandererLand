b857a61f18bf704f2a2521e7f47462296ece89d2
Ann Chou <yuanchou.ann@gmail.com>
2024-03-09 20:30:23 +0800

    fix: 修復 pagination css 錯誤 1. 使用 :deep，使子元素也能被影響 2. 原使用 .swiper 修改套件樣式，現改用 .product-swiper 樣式再最外層

--- src/components/swiperProductComponent.vue
+++ src/components/swiperProductComponent.vue
@@ -20,7 +20,7 @@
       }" :navigation="{
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
-      }" :modules="modules" class="mySwiper">
+      }" :modules="modules" class="product-swiper">
         <SwiperSlide v-for="( item, idx ) in  currentDatas " :key="idx">
           <router-link :to="`${DataInfo[dataCategory].infoPath}/${item.id}`">
             <div
@@ -139,13 +139,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.swiper {
+:deep(.product-swiper) {
   margin: auto;
   padding: 0;
 
   @media (min-width: 768px) {
     margin: 0 -20px !important;
     padding: 0 20px !important;
+  }
+
+  /* swiper 顯示區域尺寸 */
+  .swiper-wrapper {
+    margin-bottom: 36px;
+
+    @media (min-width: 768px) {
+      margin-bottom: 0rem;
+    }
   }
 
   /*Swiper 頁碼導覽*/
@@ -172,7 +181,7 @@ export default {
     background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-right-circle' viewBox='0 0 16 16'%3E%3Cpath fill-rule='evenodd' d='M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z'/%3E%3C/svg%3E") no-repeat center;
     background-size: 40px 40px;
     z-index: 11;
-    right:0;
+    right: 0;
     text-indent: 150%;
     white-space: nowrap;
     overflow: hidden;
@@ -188,7 +197,7 @@ export default {
   .swiper-button-prev {
     position: absolute;
     z-index: 1;
-    left:0;
+    left: 0;
     display: none;
     background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-left-circle' viewBox='0 0 16 16'%3E%3Cpath fill-rule='evenodd' d='M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z'/%3E%3C/svg%3E") no-repeat center;
     background-size: 40px 40px;
@@ -201,15 +210,6 @@ export default {
     @media (min-width: 768px) {
       display: flex;
     }
-  }
-}
-
-/* swiper 顯示區域尺寸 */
-.swiper-wrapper {
-  margin-bottom: 36px;
-
-  @media (min-width: 768px) {
-    margin-bottom: 0rem;
   }
 }
 </style>
