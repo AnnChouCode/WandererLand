b25083e0c61773c1a60e5fe96cb36273a0f0bd54
Ann Chou <yuanchou.ann@gmail.com>
2024-03-14 16:27:53 +0800

    feat: 新增藝術家排序篩選功能

--- src/components/userNavArtistFilter.vue
+++ src/components/userNavArtistFilter.vue
@@ -2,73 +2,215 @@
     <nav class="fixed-top z-1 bg-tertiary nav-product-filter w-100">
         <div class="container">
             <!-- web -->
-            <ul class="d-none d-lg-flex align-items-center list-unstyled">
-                <li>
-                    <div class="dropdown">
-                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
-                            id="btnProductSort" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="me-3">排序</span>
-                            <i class="bi bi-chevron-down"></i>
-                        </button>
-                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnProductSort">
-                            <li><a class="dropdown-item p-3" href="#">最新藝術家</a></li>
-                            <li><a class="dropdown-item p-3" href="#">姓名：A - Z</a></li>
-                            <li><a class="dropdown-item p-3" href="#">姓名：Z - A</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <div class="dropdown">
-                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
-                            id="btnGroupFilter" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="me-3">題材</span>
-                            <i class="bi bi-chevron-down"></i>
-                        </button>
-                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnGroupFilter">
-                            <li><a class="dropdown-item p-3" href="#">最新作品</a></li>
-                            <li><a class="dropdown-item p-3" href="#">價格：高至低</a></li>
-                            <li><a class="dropdown-item p-3" href="#">價格：低至高</a></li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <div class="dropdown">
-                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
-                            id="btnArtistFilter" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="me-3">藝術家</span>
-                            <i class="bi bi-chevron-down"></i>
-                        </button>
-                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnArtistFilter">
-                            <li><a class="dropdown-item p-3" href="#">最新作品</a></li>
-                            <li><a class="dropdown-item p-3" href="#">價格：高至低</a></li>
-                            <li><a class="dropdown-item p-3" href="#">價格：低至高</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
+            <div class="d-none d-lg-flex align-items-center">
+                <ul class="d-none d-lg-flex align-items-center list-unstyled">
+                    <li>
+                        <div class="dropdown">
+                            <button type="button"
+                                class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                                id="btnProductSort" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="me-3">排序</span>
+                                <i class="bi bi-chevron-down"></i>
+                            </button>
+                            <ul class="dropdown-menu p-0 rounded-0" aria-labelledby="btnProductSort">
+                                <li><a class="dropdown-item p-3" href="#"
+                                        @click.prevent="filterShow(['sort', 'newest'])">最新藝術家</a></li>
+                                <li><a class="dropdown-item p-3" href="#"
+                                        @click.prevent="filterShow(['sort', 'nameAToZ'])">姓名：A - Z</a></li>
+                                <li><a class="dropdown-item p-3" href="#"
+                                        @click.prevent="filterShow(['sort', 'nameZToA'])">姓名：Z - A</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <button type="button"
+                                class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                                id="btnGroupFilter" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="me-3">題材</span>
+                                <i class="bi bi-chevron-down"></i>
+                            </button>
+                            <ul class="dropdown-menu p-0 overflow-y-scroll rounded-0" aria-labelledby="btnGroupFilter"
+                                style="max-height:400px;">
+                                <li v-for="item in groupList.artistGroupList" :key="item">
+                                    <a class="dropdown-item p-3" href="#"
+                                        @click.prevent="filterShow(['group', item])">{{ item }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <button type="button"
+                                class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                                id="btnArtistFilter" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="me-3">藝術家</span>
+                                <i class="bi bi-chevron-down"></i>
+                            </button>
+                            <ul class="dropdown-menu p-0 overflow-y-scroll rounded-0" aria-labelledby="btnArtistFilter"
+                                style="max-height:400px;">
+                                <li v-for="artist in allArtists" :key="artist.id">
+                                    <a class="dropdown-item p-3" href="#"
+                                        @click.prevent="filterShow(['title', artist.title])">{{ artist.title }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+                <button class="btn btn-outline-default rounded-0 fs-info" type="button"
+                    v-if="Object.keys(filterRules).length" @click="filterShow()">清空篩選</button>
+            </div>
+
             <!-- mobile -->
-            <ul class="d-flex d-lg-none align-items-center justify-content-between list-unstyled">
-                <li>
-                    <div class="dropdown">
-                        <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
-                            id="btnProductSort" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span class="me-3">排序</span>
+            <div class="d-flex d-lg-none align-items-center justify-content-between">
+                <ul class="d-flex align-items-center justify-content-between list-unstyled">
+                    <li>
+                        <div class="dropdown">
+                            <button type="button"
+                                class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                                id="btnProductSort" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="me-3">排序</span>
+                                <i class="bi bi-chevron-down"></i>
+                            </button>
+                            <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnProductSort">
+                                <li>
+                                    <a class="dropdown-item p-3" href="#"
+                                        @click.prevent="filterShow(['sort', 'newest'])">最新藝術家</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item p-3" href="#"
+                                        @click.prevent="filterShow(['sort', 'nameAToZ'])">姓名：A - Z</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item p-3" href="#"
+                                        @click.prevent="filterShow(['sort', 'nameZToA'])">姓名：Z - A</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <button type="button"
+                            class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0"
+                            aria-controls="offcanvasFilterNav" @click="openMobileNav">
+                            <span class="me-3">篩選</span>
                             <i class="bi bi-chevron-down"></i>
                         </button>
-                        <ul class="dropdown-menu p-0 overflow-hidden" aria-labelledby="btnProductSort">
-                            <li><a class="dropdown-item p-3" href="#">最新藝術家</a></li>
-                            <li><a class="dropdown-item p-3" href="#">姓名：A - Z</a></li>
-                            <li><a class="dropdown-item p-3" href="#">姓名：Z - A</a></li>
-                        </ul>
+                    </li>
+                </ul>
+                <button class="btn btn-outline-default rounded-0 fs-info" type="button"
+                    v-if="Object.keys(filterRules).length" @click="filterShow()">清空篩選</button>
+            </div>
+        </div>
+    </nav>
+
+    <!-- mobile Nav -->
+    <div class="offcanvas offcanvas-start bg-primary" tabindex="-1" id="offcanvasNav"
+        aria-labelledby="offcanvasNavLabel" ref="mobileNav">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavLabel">WANDERER LAND</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="d-flex flex-column gap-3 gap-sm-5 list-unstyled accordion accordion-flush"
+                id="accordionFlushFilter">
+                <li class="accordion-item bg-transparent">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed px-0 text-default bg-transparent" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseGroup" aria-expanded="false" aria-controls="flush-collapseGroup">
+                            題材
+                        </button>
+                    </h2>
+                    <div id="flush-collapseGroup" class="accordion-collapse collapse"
+                        data-bs-parent="#accordionFlushGroup">
+                        <div class="accordion-body px-0 py-0">
+                            <ul class="list-unstyled" aria-labelledby="btnGroupFilter">
+                                <li v-for="item in groupList.artistGroupList" :key="item">
+                                    <a class="d-block p-3 text-info" href="#"
+                                        @click.prevent="filterShow(['group', item])">{{ item }}</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </li>
-                <li>
-                    <button type="button" class="btn d-flex align-items-center px-5 py-4 text-default fs-info border-0">
-                        <span class="me-3">篩選</span>
-                        <i class="bi bi-chevron-down"></i>
-                    </button>
+                <li class="accordion-item bg-transparent">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed px-0 text-default bg-transparent" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseArtist" aria-expanded="false" aria-controls="flush-collapseArtist">
+                            藝術家
+                        </button>
+                    </h2>
+                    <div id="flush-collapseArtist" class="accordion-collapse collapse"
+                        data-bs-parent="#accordionFlushArtist">
+                        <div class="accordion-body px-0 py-0">
+                            <ul class="list-unstyled" aria-labelledby="btnArtistFilter">
+                                <li v-for="artist in allArtists" :key="artist.id">
+                                    <a class="d-block p-3 text-info" href="#"
+                                        @click.prevent="filterShow(['title', artist.title])">{{ artist.title }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </div>
-    </nav>
+    </div>
 </template>
+
+<script>
+import { Offcanvas } from 'bootstrap'
+
+export default {
+  data () {
+    return {
+      // 篩選條件
+      filterRules: {},
+      // mobile 選單
+      mobileNav: null
+    }
+  },
+  props: ['allArtists', 'groupList'],
+  methods: {
+    // 排序篩選要顯示的資料
+    filterShow (filters) {
+      if (filters) {
+        const [key, values] = filters
+        this.filterRules[key] = values
+
+        const path = Object.entries(this.filterRules).map(item => item.join('=')).join('&')
+        this.$router.push(`/artistList?${path}`)
+        return
+      }
+
+      this.filterRules = {}
+      this.$router.push('/artistList')
+    },
+
+    // 開啟 mobile nav
+    openMobileNav () {
+      this.mobileNav.show()
+    },
+
+    // 關閉 mobile nav
+    closeMobileNav () {
+      this.mobileNav.hide()
+    }
+  },
+  mounted () {
+    // 綁定 mobile nav offcanvas
+    this.mobileNav = new Offcanvas(this.$refs.mobileNav)
+
+    // 確認頁面屬性
+    const path = window.location.href.split('?')[1]
+
+    if (!path) {
+      this.filterRules = {}
+      return
+    }
+
+    path.split('&').forEach(item => {
+      const temp = item.split('=')
+      this.filterRules[temp[0]] = temp[1]
+    })
+  }
+}
+</script>
--- src/views/user/UserArtistList.vue
+++ src/views/user/UserArtistList.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="container user-product-page-container">
+  <userNavArtistFilter :allArtists="allArtists" :groupList="groupList" @filterShow="filterShow"></userNavArtistFilter>
+  <div class="container user-product-page-container flex-grow-1">
     <h2 class="mb-7 mb-md-8 h1 lh-sm title-letter-spacing text-center h1">藝術家</h2>
-    <div class="row  g-3 g-md-8">
-      <div class="col-6 col-md-4" v-for="item in sortNewest.newestArtist" :key="item.id">
+    <div class="row g-3 g-md-8">
+      <div class="col-6 col-md-4" v-for="item in artistList" :key="item.id">
         <router-link :to="`artistInfo/${item.id}`">
           <div
             class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img rounded-circle">
@@ -22,38 +23,78 @@
 <script>
 import userProductStore from '@/stores/userProductStore.js'
 import { mapActions, mapState } from 'pinia'
+import userNavArtistFilter from '@/components/userNavArtistFilter.vue'
 
 export default {
   data () {
     return {
       // 產品列表
-      productsList: [],
-      // 頁碼
-      pagination: {}
+      artistList: []
     }
   },
   methods: {
     // 取得所有產品資料，生成產品與分類資料
     ...mapActions(userProductStore, ['getAllProducts']),
 
-    // 切換產品資訊頁
-    showProductInfo (id) {
-      this.$router.push(`/productinfo/${id}`)
+    filterShow (filters) {
+      // 獲取排序篩選條件
+      const { sort, ...filterConditions } = filters
+
+      // 初始化暫時產品列表
+      let filteredArtists = this.sortNewest.newestArtist
+
+      // 篩選產品
+      Object.entries(filterConditions).forEach(([key, value]) => {
+        if (value === '' || value === undefined) {
+          return
+        }
+
+        filteredArtists = filteredArtists.filter(product => {
+          return product[key] === value
+        })
+      })
+
+      // 排序
+      if (sort === 'newest') {
+        filteredArtists = filteredArtists.sort((a, b) => b.create_at - a.create_at)
+      } else if (sort === 'nameAToZ') {
+        filteredArtists = filteredArtists.sort((a, b) => a.title.localeCompare(b.title))
+      } else if (sort === 'nameZToA') {
+        filteredArtists = filteredArtists.sort((a, b) => b.title.localeCompare(a.title))
+      }
+
+      // 更新產品列表
+      this.artistList = filteredArtists
     }
   },
-  mounted () {
-    // 取得分頁產品列表
-    // this.getProductList()
+  watch: {
+    $route () {
+      const filters = {
+        sort: this.$route.query.sort,
+        title: this.$route.query.title,
+        group: this.$route.query.group
+      }
+
+      this.filterShow(filters)
+    }
+  },
+  async mounted () {
     // 取得所有產品資料
-    this.getAllProducts()
+    await this.getAllProducts()
+
+    const filters = {
+      sort: this.$route.query.sort,
+      artist: this.$route.query.artist,
+      group: this.$route.query.group
+    }
+
+    this.filterShow(filters)
   },
   computed: {
-    ...mapState(userProductStore, ['sortNewest'])
+    ...mapState(userProductStore, ['allArtists', 'sortNewest', 'groupList'])
   },
   components: {
-    // userNavProductFilter
-    // paginationComponent,
-    // filterCategory
+    userNavArtistFilter
   }
 }
 </script>
