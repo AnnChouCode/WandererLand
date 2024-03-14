003ca8fb47330278b639c05ffb0af848f439b882
Ann Chou <yuanchou.ann@gmail.com>
2024-03-14 11:57:36 +0800

    feat: 新增產品分類生成、新增產品尺寸分類表功能、更新最新藝術家產品排序

--- src/stores/userProductStore.js
+++ src/stores/userProductStore.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 import swal from 'sweetalert2'
+import { useLoading } from 'vue-loading-overlay'
+const $loading = useLoading()
 
 const { VITE_API, VITE_PATH } = import.meta.env
 
@@ -8,20 +10,28 @@ export default defineStore('allProductsStore', {
   state: () => ({
     // 所有產品
     allProducts: [],
-    // 產品分類列表
-    categoryList: []
+    // 所有藝術家
+    allArtists: [],
+    // 產品尺寸表
+    productSizeList: [],
+    // filter
+    filter: []
   }),
   actions: {
     // 獲取所有產品
     getAllProducts () {
+      // 開啟 loading
+      const loader = $loading.show()
+
       const url = `${VITE_API}/api/${VITE_PATH}/products/all`
 
       return axios.get(url)
         .then(res => {
-          this.allProducts = res.data.products
-          this.getCategoryList()
+          const allDatas = res.data.products
+          this.allProducts = allDatas.filter(item => item.category === 'products')
+          this.allArtists = allDatas.filter(item => item.category === 'artists').sort((a, b) => b.title.localeCompare(a.title))
 
-          return this.allProducts
+          return allDatas
         })
         .catch(err => {
           swal.fire(
@@ -31,23 +41,42 @@ export default defineStore('allProductsStore', {
             }
           )
         })
+        .finally(() => {
+          // 關閉 loading
+          loader.hide()
+        })
     },
 
-    // 取得產品分類列表
-    getCategoryList () {
-      const categoryList = new Set(this.allProducts.map(item => item.category))
-      this.categoryList = [...categoryList]
+    // 取得產品尺寸表
+    getSizeList () {
+      const tempSizeList = new Set(this.allProducts.map(item => item.size))
+      const sortSize = [...tempSizeList].sort((a, b) => a[0] - b[0])
+
+      this.productSizeList = sortSize
     }
   },
   getters: {
-    sortNewest: ({ allProducts }) => {
-      const newestArtist = allProducts.filter(item => item.category === 'artists').reverse()
-      const newestProduct = allProducts.filter(item => item.category === 'products').reverse()
+    // 排序最新產品、藝術家
+    sortNewest: ({ allProducts, allArtists }) => {
+      const newestArtist = allArtists.sort((a, b) => b.create_at - a.create_at)
+      const newestProduct = allProducts.sort((a, b) => b.create_at - a.create_at)
 
       return {
         newestArtist,
         newestProduct
       }
+    },
+    groupList: ({ allProducts, allArtists }) => {
+      const tempProductGroupList = new Set(allProducts.map(item => item.group))
+      const productGroupList = [...tempProductGroupList]
+      const tempArtistGroupList = new Set(allArtists.map(item => item.group))
+      const artistGroupList = [...tempArtistGroupList]
+
+      return {
+        productGroupList,
+        artistGroupList
+      }
     }
+    // getGroupLis: ({}) => {}
   }
 })
