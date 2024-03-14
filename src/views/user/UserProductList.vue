02b0c03d7e810ffb210538929600a8591a3196a6
Ann Chou <yuanchou.ann@gmail.com>
2024-03-14 11:56:01 +0800

    feat: 引入產品 navbar 元件、新增產品篩選功能

--- src/views/user/UserProductList.vue
+++ src/views/user/UserProductList.vue
@@ -1,10 +1,10 @@
 <template>
-  <!-- <userNavProductFilter></userNavProductFilter> -->
-
-  <div class="container user-product-page-container">
+  <userNavProductFilter :allArtists="allArtists" :groupList="groupList" :productSizeList="productSizeList"
+    @filterShow="filterShow"></userNavProductFilter>
+  <div class="container user-product-page-container flex-grow-1">
     <h2 class="mb-7 mb-md-8 h1 lh-sm title-letter-spacing text-center h1">作品</h2>
     <div class="row  g-3 g-md-8">
-      <div class="col-6 col-md-4" v-for="item in sortNewest.newestProduct" :key="item.id">
+      <div class="col-6 col-md-4" v-for="item in productsList" :key="item.id">
         <router-link :to="`productInfo/${item.id}`">
           <div
             class="position-relative d-flex justify-content-center align-items-center ratio-1x1 overflow-hidden product-img">
@@ -16,7 +16,7 @@
         <div class="py-3 px-0 px-md-4">
           <router-link :to="`productInfo/${item.id}`">
             <h3 class="mb-2 fs-info fs-md-5 fw-bold text-default">{{
-        item.title }}</h3>
+    item.title }}</h3>
           </router-link>
           <div class="d-flex justify-content-between align-items-center">
             <p class="fs-info fs-md-6 text-info">NT$ {{ item.price.toLocaleString() }}</p>
@@ -31,11 +31,10 @@
 <script>
 import userProductStore from '@/stores/userProductStore.js'
 import { mapActions, mapState } from 'pinia'
+import userNavProductFilter from '@/components/userNavProductFilter.vue'
 
 // Import Components
 import btnFavorite from '@/components/btnFavorite.vue'
-
-// const { VITE_API, VITE_PATH } = import.meta.env
 
 export default {
   data () {
@@ -48,22 +47,77 @@ export default {
   },
   methods: {
     // 取得所有產品資料，生成產品與分類資料
-    ...mapActions(userProductStore, ['getAllProducts']),
+    ...mapActions(userProductStore, ['getAllProducts', 'getSizeList']),
 
     // 切換產品資訊頁
     showProductInfo (id) {
       this.$router.push(`/productinfo/${id}`)
+    },
+
+    filterShow (filters) {
+      // 獲取排序篩選條件
+      const { sort, ...filterConditions } = filters
+
+      // 初始化暫時產品列表
+      let filteredProducts = this.sortNewest.newestProduct
+
+      // 篩選產品
+      Object.entries(filterConditions).forEach(([key, value]) => {
+        if (value === '' || value === undefined) {
+          return
+        }
+
+        filteredProducts = filteredProducts.filter(product => {
+          return product[key] === value
+        })
+      })
+
+      // 排序
+      if (sort === 'newest') {
+        filteredProducts = filteredProducts.sort((a, b) => b.create_at - a.create_at)
+      } else if (sort === 'priceHighToLow') {
+        filteredProducts = filteredProducts.sort((a, b) => b.price - a.price)
+      } else if (sort === 'priceLowToHigh') {
+        filteredProducts = filteredProducts.sort((a, b) => a.price - b.price)
+      }
+
+      // 更新產品列表
+      this.productsList = filteredProducts
     }
   },
-  mounted () {
+  watch: {
+    $route () {
+      const filters = {
+        sort: this.$route.query.sort,
+        artist: this.$route.query.artist,
+        group: this.$route.query.group,
+        size: this.$route.query.size
+      }
+
+      this.filterShow(filters)
+    }
+  },
+  async mounted () {
     // 取得所有產品資料
-    this.getAllProducts()
+    await this.getAllProducts()
+
+    const filters = {
+      sort: this.$route.query.sort,
+      artist: this.$route.query.artist,
+      group: this.$route.query.group,
+      size: this.$route.query.size
+    }
+
+    this.filterShow(filters)
+
+    this.getSizeList()
   },
   computed: {
-    ...mapState(userProductStore, ['sortNewest'])
+    ...mapState(userProductStore, ['allArtists', 'sortNewest', 'groupList', 'productSizeList'])
   },
   components: {
-    btnFavorite
+    btnFavorite,
+    userNavProductFilter
   }
 }
 </script>
