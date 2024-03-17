2bc25b6efbb40b36001fe0e8603cb9304afcb472
Ann Chou <yuanchou.ann@gmail.com>
2024-03-17 21:24:09 +0800

    feat: 調整 toast message 層級

--- src/components/toastComponent.vue
+++ src/components/toastComponent.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 1056">
     <div class="toast show mb-1 bg-tertiary" role="alert" aria-live="assertive" aria-atomic="true"
       v-for="(item, idx) in toastMsgList" :key="idx">
       <div class="toast-body d-flex align-items-center justify-content-between">
-        <div class="d-flex">
-          <i class="bi me-2 text-success" :class="item.icon"></i>
+        <div class="d-flex fw-bold">
+          <i class="bi me-2 text-default" :class="item.icon"></i>
           <p class="mb-0">{{ item.msg }}</p>
         </div>
         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"
