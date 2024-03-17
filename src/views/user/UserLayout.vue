aba7a87eee9bfe8ae20bb76d955f4f7991c70bd3
Ann Chou <yuanchou.ann@gmail.com>
2024-03-17 21:23:46 +0800

    feat: 引入回到上方按鈕、刪除多餘註解程式碼

--- src/views/user/UserLayout.vue
+++ src/views/user/UserLayout.vue
@@ -1,42 +1,10 @@
 <template>
-    <!-- <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a href="#/home">
-        <h1 class="mb-0 me-5 fs-4 text-white">Dessert Time</h1>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav justify-content-between w-100">
-          <li class="nav-item d-flex flex-column flex-lg-row gap-3 py-2">
-            <RouterLink to="/productlist" class="text-secondary">看產品</RouterLink>
-            <RouterLink to="/orders" class="text-secondary">查訂單</RouterLink>
-          </li>
-          <li class="nav-item d-flex flex-column flex-lg-row gap-3 py-2">
-            <RouterLink to="/cart" class="position-relative text-secondary">
-              <p>購物車</p>
-              <div v-if="cartsList.total" class="position-absolute top-0 start-100 translate-middle badge bg-danger rounded-pill px-2 py-1 text-white">{{ cartsList.carts.length }}</div>
-            </RouterLink>
-
-            <RouterLink v-if="!isLoggedin" to="/userlogin" class="text-secondary">
-              登入
-            </RouterLink>
-
-            <RouterLink v-else to="/admin" class="text-secondary">
-              後台
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav> -->
   <div class="d-flex flex-column dvh-100">
     <userNavBar></userNavBar>
     <RouterView />
     <userFooter :isLoggedin="isLoggedin"></userFooter>
-    <toastComponent></toastComponent>
+    <toastComponent class="position-fixed z-3 end-0" style="bottom:45px;"></toastComponent>
+    <btnBackToTop  class="position-fixed z-3 bottom-0 end-0"></btnBackToTop>
   </div>
 </template>
 
@@ -44,6 +12,7 @@
 import userNavBar from '@/components/userNavBar.vue'
 import userFooter from '@/components/userFooter.vue'
 import toastComponent from '@/components/toastComponent.vue'
+import btnBackToTop from '@/components/btnBackToTop.vue'
 import cartStore from '@/stores/userCartStore.js'
 import { mapActions } from 'pinia'
 
@@ -74,7 +43,8 @@ export default {
   components: {
     toastComponent,
     userNavBar,
-    userFooter
+    userFooter,
+    btnBackToTop
   }
 }
 </script>
