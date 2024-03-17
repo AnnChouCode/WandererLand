efe3045990c2487b0d203b5417868ca7f98d8b26
Ann Chou <yuanchou.ann@gmail.com>
2024-03-17 22:51:18 +0800

    style: 更改 icon 樣式

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,10 +2,10 @@
     <div class="bg-light">
       <div class="container vh-100 py-5">
         <div class="d-flex flex-column align-items-center">
-          <img class="mb-4 size-100" src="https://3.bp.blogspot.com/-KsqNNr4cQME/VWmA79jIxfI/AAAAAAAAt2A/JYlg4d6inMY/s800/mark_manpu04_question.png" alt="404 Not Found">
+          <i class="my-7 bi bi-question-circle h1"></i>
           <h2 class="fw-bold mb-4 fs-1"> 404 Not Found</h2>
-          <p class="text-center fw-bold fs-4 mb-4">你要找的頁面好像不在這裡</p>
-          <router-link to="/home" class="btn btn-dark fs-5 text-white">回到首頁</router-link>
+          <p class="text-center fw-bold fs-4 mb-7">你要找的頁面好像不在這裡</p>
+          <router-link to="/home" class="btn btn-dark fs-5 text-white rounded-0">回到首頁</router-link>
         </div>
       </div>
     </div>
