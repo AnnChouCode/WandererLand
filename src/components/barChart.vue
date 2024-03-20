2442f44164b5f4203803806346225512f240f85e
Ann Chou <yuanchou.ann@gmail.com>
2024-03-20 17:14:45 +0800

    feat: 新增銷售額數據圖表

---
+++ src/components/barChart.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="height:300px">
+    <h3 class="mb-2 fs-6 fw-bold text-info">總銷售額</h3>
+    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  </div>
+</template>
+
+<script>
+// 引入
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+// 註冊
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'barChart',
+  components: { Bar },
+  props: {
+    chartData: Object,
+    chartOptions: Object
+  }
+}
+</script>
--- src/views/admin/AdminDashBoard.vue
+++ src/views/admin/AdminDashBoard.vue
@@ -1,40 +1,47 @@
 <template>
-    <div class="overflow-y-scroll scrollbar-y-hide vh-100">
-        <h2 class="py-4 py-md-7 fw-bold">總覽</h2>
-        <div class="row g-1 p-2 p-md-3 bg-light">
-            <div class="col-12 col-md-3">
-                <div class="p-3 ">
-                    <h3 class="mb-1 fs-5 fw-bold">歡迎回來</h3>
-                    <p>這是您的待辦事項</p>
-                </div>
-            </div>
-            <div class="col-12 col-md-3">
-                <div class="p-3 border rounded-2 bg-white">
-                    <p class="fs-3">{{isNotPaidOrder.length}}</p>
-                    <h3 class="fs-6 fw-bold text-info">尚未付款的訂單</h3>
-                </div>
-            </div>
-            <div class="col-12 col-md-3">
-                <div class="p-3 border rounded-2 bg-white">
-                    <p class="fs-3">{{isNotShippingOrder.length}}</p>
-                    <h3 class="fs-6 fw-bold text-info">待出貨的訂單</h3>
-                </div>
-            </div>
-            <div class="col-12 col-md-3">
-                <div class="p-3 border rounded-2 bg-white">
-                    <p class="fs-3">{{needReplenishedProduct.length}}</p>
-                    <h3 class="fs-6 fw-bold text-info">即將售鑿商品</h3>
-                </div>
-            </div>
+  <div class="overflow-y-scroll scrollbar-y-hide vh-100">
+    <h2 class="py-4 py-md-7 fw-bold">總覽</h2>
+    <div class="row g-1 p-2 p-md-3 mb-4 bg-light">
+      <div class="col-12 col-md-3">
+        <div class="p-3 ">
+          <h3 class="mb-1 fs-5 fw-bold">歡迎回來</h3>
+          <p>這是您的待辦事項</p>
         </div>
-
+      </div>
+      <div class="col-12 col-md-3">
+        <div class="p-3 border rounded-2 bg-white">
+          <p class="fs-3">{{ isNotPaidOrder.length }}</p>
+          <h3 class="fs-6 fw-bold text-info">尚未付款的訂單</h3>
+        </div>
+      </div>
+      <div class="col-12 col-md-3">
+        <div class="p-3 border rounded-2 bg-white">
+          <p class="fs-3">{{ isNotShippingOrder.length }}</p>
+          <h3 class="fs-6 fw-bold text-info">待出貨的訂單</h3>
+        </div>
+      </div>
+      <div class="col-12 col-md-3">
+        <div class="p-3 border rounded-2 bg-white">
+          <p class="fs-3">{{ needReplenishedProduct.length }}</p>
+          <h3 class="fs-6 fw-bold text-info">即將售鑿商品</h3>
+        </div>
+      </div>
     </div>
+    <div class="p-2 p-md-3 bg-light">
+      <div class="w-100 p-3 border rounded-2 bg-white" v-if="Object.values(chartData).length">
+        <barChart :chartOptions="chartOptions" :chartData="chartData"></barChart>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { useAdminOrderStore } from '@/stores/adminOrderStore'
 import { useAdminProductsStore } from '@/stores/adminProductStore'
 import { mapStores } from 'pinia'
+
+// 引入 barChart 子元件
+import barChart from '@/components/barChart.vue'
 
 const orderStore = useAdminOrderStore()
 const productStore = useAdminProductsStore()
@@ -51,32 +58,103 @@ export default {
       // 未出貨的已付款訂單
       isNotShippingOrder: [],
       // 需補貨，即將售完商品
-      needReplenishedProduct: []
+      needReplenishedProduct: [],
+      chartMonthZone: 3,
+      // 圖表資料
+      chartData: {},
+      // 圖表設定
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
     }
   },
   computed: {
     ...mapStores(useAdminProductsStore),
     ...mapStores(useAdminOrderStore)
   },
+  watch: {
+    chartMonthZone () {
+      this.handleChartData(this.chartMonthZone)
+    }
+  },
   methods: {
+    // 圖表數據
+    handleChartData (monthZone = 12) {
+      // 獲取月份表
+      const currentDate = new Date() // 現在的日期
+      const currentMonth = currentDate.getMonth() + 1 // 獲取當前月份
+      const currentYear = currentDate.getFullYear() // 獲取當前年份
+
+      // 前三個月的月份
+      const previousMonths = []
+      for (let i = 0; i < monthZone; i++) {
+        let previousMonth = currentMonth - i
+        let previousYear = currentYear
+
+        if (previousMonth <= 0) {
+          previousMonth += 12
+          previousYear--
+        }
+
+        previousMonths.unshift(`${previousYear.toString().slice(2, 4)} ${previousMonth}月`)
+      }
+
+      this.chartData.labels = previousMonths
+
+      const turnoverMap = {}
+
+      orderStore.allOrderList.forEach(order => {
+        const formatDate = new Date(order.create_at * 1000).toLocaleString()
+        const date = formatDate.split('/').splice(0, 2)
+        const matchDate = `${date[0].slice(2, 4)} ${date[1]}月`
+
+        if (!turnoverMap[matchDate]) {
+          turnoverMap[matchDate] = 0
+        }
+
+        Object.values(order.products).forEach(product => {
+          turnoverMap[matchDate] += product.total
+        })
+      })
+
+      const turnoverResult = previousMonths.map(month => {
+        return turnoverMap[month] || 0
+      })
+
+      this.chartData.datasets = [
+        {
+          label: '總銷售額',
+          backgroundColor: '#333333',
+          data: turnoverResult
+        }
+      ]
+    },
+
+    // 訂單數據
     handleOrderData () {
       this.isNotPaidOrder = this.allOrderList.filter(order => !order.is_paid)
       this.isNotShippingOrder = this.allOrderList.filter(order => order.is_paid && !order.is_shipping)
     },
 
+    // 產品數據
     handleProductData () {
       this.needReplenishedProduct = this.allProductList.filter(product => product.quantity && product.quantity <= 3)
     },
 
+    // 獲得所有訂單
     async getAllOrderList () {
       await orderStore.getCurrentOrderList()
       for (let i = 0; i < orderStore.pagination.total_pages; i++) {
         await orderStore.getAllOrderList(i + 1)
       }
       this.allOrderList = orderStore.allOrderList
+
       this.handleOrderData()
+      this.handleChartData()
     },
 
+    // 獲得所有產品
     async getAllProductList () {
       await productStore.getAllProducts()
       this.allProductList = Object.values(productStore.allProducts)
@@ -89,6 +167,9 @@ export default {
 
     this.getAllOrderList()
     this.getAllProductList()
+  },
+  components: {
+    barChart
   }
 }
 </script>
