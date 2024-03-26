831ef01da9907a64a7ca70be4047390b5a1e1925
Ann Chou <yuanchou.ann@gmail.com>
2024-03-26 17:09:55 +0800

    style: 新增 hover 底線動畫

--- src/components/button/BtnSeeMore.vue
+++ src/components/button/BtnSeeMore.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link class="text-default border-bottom border-default fw-bold fs-info fs-md-6" :to="linkTo">瀏覽更多</router-link>
+    <router-link class="text-default fw-bold fs-info fs-md-6 btn-see-more" :to="linkTo">瀏覽更多</router-link>
 </template>
 
 <script>
@@ -10,3 +10,30 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.btn-see-more {
+  position:relative;
+  transition: border 0.3s;
+  border-bottom: 1px solid #333333;
+
+  &::after{
+    position: absolute;
+    top:0;
+    left:0;
+    content:'';
+    width:0%;
+    height: 100%;
+    border-bottom: 1px solid #333333;
+    transition: width 0.5s;
+  }
+
+  &:hover{
+    border-bottom: 1px solid transparent;
+
+    &::after{
+      width: 100%;
+    }
+  }
+}
+</style>
