7e099b208cf1798cd43373b663f7a4fc76d4abdb
Ann Chou <yuanchou.ann@gmail.com>
2024-03-27 17:31:11 +0800

    feat: 刪除首頁 coupon 區塊元件，改為 HTML 寫入首頁

--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -144,7 +144,16 @@
   </div>
 
   <!-- 優惠券 -->
-  <couponComponent />
+  <div class="position-relative d-flex justify-content-center align-items-center w-100 home-coupon overflow-hidden bg-primary">
+    <div class="position-absolute top-50 z-1 translate-middle-y text-center title-shadow">
+      <h2 class="mb-2 fs-4 fs-md-1 text-white title-letter-spacing">使用優惠券</h2>
+      <p class="mb-7 fs-info fs-md-6 text-white">為你的第一筆訂單，獲取 97 折折扣</p>
+      <button type="button" class="py-2 py-md-3 px-6 px-md-9 btn btn-light fw-bold rounded-0"
+        @click="copyCouponCode('present97')">點擊複製優惠碼</button>
+    </div>
+    <img src="../../../public/images/present.jpg" alt="coupon"
+      class="object-fit-cover w-100 h-100" data-aos="zoom-out" data-aos-duration="3000">
+  </div>
 
   <div class="bg-white">
     <div class="container">
@@ -228,11 +237,11 @@
 
 <script>
 import articleStore from '@/stores/userArticleStore.js'
+import cartStore from '@/stores/userCartStore.js'
 import { mapActions, mapState } from 'pinia'
 
 // Import Components
 import SwiperProductComponent from '@/components/swiper/SwiperProductComponent.vue'
-import CouponComponent from '@/components/CouponComponent.vue'
 import BtnSeeMore from '@/components/button/BtnSeeMore.vue'
 
 export default {
@@ -261,6 +270,8 @@ export default {
   methods: {
     // 獲得購物車資料
     ...mapActions(articleStore, ['getArticlesList']),
+    // 複製優惠碼動作
+    ...mapActions(cartStore, ['copyCouponCode']),
 
     sendEmail () {
       const isCompletedForm = Object.values(this.message).every(value => value !== '')
@@ -272,7 +283,6 @@ export default {
   },
   components: {
     SwiperProductComponent,
-    CouponComponent,
     BtnSeeMore
   },
   computed: { ...mapState(articleStore, ['sortArticles', 'articlesList']) },
@@ -357,6 +367,15 @@ export default {
       transform: translate(-50%, -50%);
       transition: border 0.3s;
     }
+  }
+}
+
+/* 優惠券 */
+.home-coupon {
+  min-height: 302px;
+
+  @media (min-width: 768px) {
+    min-height: 311px;
   }
 }
 </style>
