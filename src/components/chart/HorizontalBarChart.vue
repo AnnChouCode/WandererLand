454645e7cfd180851b5f46e9379f312f038d9ec5
Ann Chou <yuanchou.ann@gmail.com>
2024-04-08 21:31:21 +0800

    feat: 新增分類銷售額橫條圖

---
+++ src/components/chart/HorizontalBarChart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="height:300px">
+    <h3 class="mb-2 fs-6 fw-bold text-info">分類銷售額</h3>
+    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  </div>
+</template>
+
+<script>
+// 引入
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+// 註冊
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'HorizontalBarChart',
+  components: { Bar },
+  props: {
+    chartData: Object
+  },
+  data () {
+    return {
+      // 圖表設定
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        indexAxis: 'y'
+      }
+    }
+  },
+  mounted () {
+    console.log(this.chartData)
+  }
+}
+</script>
